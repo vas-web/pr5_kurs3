393ef70f5b9fac7477d71f77c1d04c9c7fce172a
исп-31 <-31@LAB44-03>
2023-12-11 14:39:05 +0300

    Добавьте файлы проекта.

---
+++ Library/Triad.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Library
+{
+    public class Triad
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int c { get; set; }
+        int aplusone()
+        {
+            return a + 1;
+        }
+        int bplusone()
+        {
+            return b + 1;
+        }
+        int cplusone()
+        {
+            return c + 1;
+        }
+    }
+}
