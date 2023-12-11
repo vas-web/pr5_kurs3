59a3575be5282cbcc18ef9edc8e769978bf19d7a
vas-web <nikarah123000@gmail.com>
2023-12-11 21:45:10 +0300

    э

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,5 +25,42 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+        public int a, b, c, a1, b1, c1;
+
+        public void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Triad triad = new Triad();
+            triad.SetParams(a1, b1, c1, true);
+            triad.Result(out a, out b, out c);
+            d.Text = Convert.ToString(a);
+            e_tb.Text = Convert.ToString(b);
+            f.Text = Convert.ToString(c);
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+
+            Triad triad = new Triad();
+
+            a1 = Convert.ToInt32(a_tb.Text);
+            b1 = Convert.ToInt32(b_tb.Text);
+            c1 = Convert.ToInt32(c_tb.Text);
+
+            triad.SetParams(a1, b1, c1);
+
+            triad.Result(out a, out b, out c);
+
+            
+
+            d.Text = Convert.ToString(a);
+            e_tb.Text = Convert.ToString(b);
+            f.Text = Convert.ToString(c);
+         
+            
+        }
+
+        
     }
 }
--- WpfApp1/Triad.cs
+++ WpfApp1/Triad.cs
@@ -1,19 +1,16 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Library
+namespace WpfApp1
 {
     public class Triad
     {
         public int A { get; set; }
         public int B { get; set; }
         public int C { get; set; }
-        public Triad(int a, int b, int c)
-        {
-            A = a;
-            B = b;
-            C = c;
-        }
-
         public void Result(out int oA, out int oB, out int oC)
         {
             oA = A;
@@ -24,9 +21,9 @@ namespace Library
         {
             if (abc)
             {
-                A = a+10;
-                B = b+10;
-                C = c+10;
+                A = a + 10;
+                B = b + 10;
+                C = c + 10;
             }
         }
         public void SetParams(int a, int b, int c)
